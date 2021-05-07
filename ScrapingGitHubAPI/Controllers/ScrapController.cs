234927c85ab6955b9d6f74d928fac7f7d25a17bd
diegolanes@outlook.com <diegolanes@outlook.com>
2021-05-07 00:13:45 -0300

    creation of api project + configuration of endpoint

---
+++ ScrapingGitHubAPI/Controllers/ScrapController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ScrapingGitHubAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ScrapController : ControllerBase
+    {
+        // GET api/values
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return @"To hit the endpoint use the url: https://localhost:44339/api/scrap/{user}/{repositorio}";
+        }
+        [HttpGet("{user}/{repo}")]
+        public ActionResult<string> Get(string user, string repo)
+        {            
+            return "value";
+        }
+    }
+}
